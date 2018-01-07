6ae8cae12e3d4ebae5a211a5f3cfb01a7414b4df
TaylorBruening <31892579+TaylorBruening@users.noreply.github.com>
2018-01-06 21:48:34 -0500

    Update pygame_example.py

--- pygame_example.py
+++ pygame_example.py
@@ -16,15 +16,25 @@ gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('A bit Racey')
 clock = pygame.time.Clock()
 
-crashed = False
+carIMG = pygame.image.load('racecar.png')
+carIMG = pygame.transform.scale(carIMG,(100,160))
 
+def car(x,y):
+	gameDisplay.blit(carIMG,(x,y))
+
+x = (display_width * 0.45)
+y = (display_height * 0.8)
+crashed = False
 while not crashed:
 	
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT: 
 			crashed = True
-
-	pygame.display.update()
+	
+	gameDisplay.fill(white)
+	car(x,y)
+	
+	pygame.display.update() #update what has been drawn to the screen
 	clock.tick(60)
 
 pygame.quit()
