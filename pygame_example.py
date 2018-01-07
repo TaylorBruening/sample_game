49ef0879af7f8d539ac6115e2d08706668dca0c8
TaylorBruening <31892579+TaylorBruening@users.noreply.github.com>
2018-01-06 22:18:42 -0500

    Update pygame_example.py

--- pygame_example.py
+++ pygame_example.py
@@ -22,20 +22,52 @@ carIMG = pygame.transform.scale(carIMG,(100,160))
 def car(x,y):
 	gameDisplay.blit(carIMG,(x,y))
 
-x = (display_width * 0.45)
-y = (display_height * 0.8)
-crashed = False
-while not crashed:
-	
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT: 
-			crashed = True
-	
-	gameDisplay.fill(white)
-	car(x,y)
-	
-	pygame.display.update() #update what has been drawn to the screen
-	clock.tick(60)
+def game_loop():
+	x = (display_width * 0.45)
+	y = (display_height * 0.8)
+
+	x_change = 0
+
+	left = False
+	right = False
+	crashed = False
+
+
+	while not crashed:
+	    for event in pygame.event.get():
+
+		if event.type == pygame.QUIT:
+		    crashed = True
+
+		if event.type == pygame.KEYDOWN:
+		    if event.key == pygame.K_LEFT:
+			left = True
+
+		    if event.key == pygame.K_RIGHT:
+			right = True
+
+		if event.type == pygame.KEYUP:
+		    if left and event.key == pygame.K_LEFT:
+			left = False
+		    if right and event.key == pygame.K_RIGHT:
+			right = False
+
+	    if left and right:
+		x_change *= 1 (stays the same)
+	    elif left:
+		x_change = -5
+	    elif right:
+		x_change = 5
+	    else:
+		x_change = 0
+
+	    x += x_change
+
+		gameDisplay.fill(white)
+		car(x,y)
+
+		pygame.display.update() #update what has been drawn to the screen
+		clock.tick(60)
 
 pygame.quit()
 quit()
